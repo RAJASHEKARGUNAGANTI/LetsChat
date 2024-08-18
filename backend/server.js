6157c07774dd3c6541f8163bb01b94e3f0078d9a
RAJASHEKARGUNAGANTI <gunagantirajashekar@gmail.com>
2024-08-18 23:27:59 +0530

    setup for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
+import path from "path"
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
-
 import connectMongDB from "./db/connnectMongoDB.js"
 import {app, server} from "./socket/socket.js"
 
@@ -12,19 +12,22 @@ import {app, server} from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); 
 app.use(cookieParser());
 
-// app.get('/', (req, res) => {
-//     res.send("Server is ready")
-// })
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*",(req, res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,()=>{
     connectMongDB();
